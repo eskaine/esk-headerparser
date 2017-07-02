b76a2c6b9c1ded5b4aa0231117818d153421db6c
System <none>
2017-07-02 04:28:38 +0000

    🎭🍋 Updated with Glitch

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
+  
+    // parse request with parser module
     var parsedData = parser(request);
+  
     response.send(JSON.stringify(parsedData));
+  
 });
 
 app.listen(port);
